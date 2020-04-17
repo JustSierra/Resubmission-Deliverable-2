09e4610f61075179fd95b6803cf30073251fa466
JustSierra <63554135+JustSierra@users.noreply.github.com>
2020-04-16 21:55:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                Console.Write("What is the message?");
+
+                var input = Console.ReadLine();
+
+                var allCaps = input.ToUpper();
+
+                string message = string.Empty;
+                //string unicodeValue = string.Empty;
+                var checksum = 0;
+
+                foreach (char x in allCaps)
+                {
+                    message += ((int)x) - 64 + "-"; 
+                    //unicodeValue += ((int)x);
+                    checksum += ((int)x);
+
+                }
+                Console.WriteLine("Your encoded message is " + message);
+
+                Console.WriteLine("Message checksum is " + checksum);
+
+
+                Console.ReadLine();
+
+
+
+            }
+
+
+
+        }
+    }
+}
+
